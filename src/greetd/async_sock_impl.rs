4306d590a96cc4690d6a1b0d85cdb3eba922d291
max-ishere <47008271+max-ishere@users.noreply.github.com>
2024-09-08 20:03:30 +0300

    docs: Add developer docs to greetd

--- src/greetd/async_sock_impl.rs
+++ src/greetd/async_sock_impl.rs
@@ -1,18 +1,21 @@
+//! An implementation of greetd client for types that can do async IO.
+//!
+//! ## Logging policy
+//!
+//! This module uses the debug level for logging in order to not store a long authentication history in real use.
 use std::marker::Unpin;
 
 use greetd_ipc::{codec::TokioCodec, AuthMessageType, ErrorType, Request, Response};
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
-use tracing::info;
+use tokio::io::{AsyncRead, AsyncWrite};
+use tracing::debug;
 
-use crate::greetd::RequestError;
+use crate::{greetd::RequestError, greetd_response};
 
-use super::{
-    AuthInformativeResponse, AuthQuestionResponse, AuthResponse, CancellableSession,
-    CreateSessionResponse, Greetd, StartableSession,
-};
+use super::{AuthResponse, CancellableSession, CreateSessionResponse, Greetd, StartableSession};
 
-pub(crate) trait TokioRW: AsyncReadExt + AsyncWriteExt + Unpin + Send {}
-impl<T> TokioRW for T where T: AsyncReadExt + AsyncWriteExt + Unpin + Send {}
+/// A marker trait for types that can do async IO.
+pub(crate) trait TokioRW: AsyncRead + AsyncWrite + Unpin + Send {}
+impl<T> TokioRW for T where T: AsyncRead + AsyncWrite + Unpin + Send {}
 
 pub struct AuthMessage<RW>
 where
@@ -51,21 +54,6 @@ where
     }
 }
 
-impl<RW> AuthQuestionResponse for AuthMessage<RW>
-where
-    RW: TokioRW,
-{
-    type Client = RW;
-}
-
-impl<RW> AuthInformativeResponse for AuthMessage<RW>
-where
-    RW: TokioRW,
-{
-    type Client = RW;
-}
-
-#[async_trait]
 impl<RW> Greetd for RW
 where
     RW: TokioRW,
@@ -76,45 +64,43 @@ where
     type AuthInformative = AuthMessage<RW>;
     type Error = greetd_ipc::codec::Error;
 
-    async fn create_session(
-        self,
-        username: &str,
-    ) -> super::Response<Self, Self, CreateSessionResponse<Self>> {
-        info!("Creating session for user: {username}");
+    fn create_session(self, username: &str) -> greetd_response!(Self, CreateSessionResponse<Self>) {
+        async move {
+            debug!("Creating session for user: {username}");
 
-        let (self_, response) = make_request(
-            self,
-            Request::CreateSession {
-                username: username.to_string(),
-            },
-        )
-        .await?;
-
-        Ok(match response {
-            Response::Success => Ok(CreateSessionResponse::Success(self_)),
-            Response::Error {
-                error_type,
-                description,
-            } => Err((
-                self_,
-                match error_type {
-                    ErrorType::Error => RequestError::Error(description),
-                    ErrorType::AuthError => RequestError::Auth(description),
+            let (self_, response) = make_request(
+                self,
+                Request::CreateSession {
+                    username: username.to_string(),
                 },
-            )),
-            Response::AuthMessage {
-                auth_message_type,
-                auth_message,
-            } => Ok(AuthMessage::new_as_create_session(
-                self_,
-                auth_message_type,
-                auth_message,
-            )),
-        })
+            )
+            .await?;
+
+            Ok(match response {
+                Response::Success => Ok(CreateSessionResponse::Success(self_)),
+                Response::Error {
+                    error_type,
+                    description,
+                } => Err((
+                    self_,
+                    match error_type {
+                        ErrorType::Error => RequestError::Error(description),
+                        ErrorType::AuthError => RequestError::Auth(description),
+                    },
+                )),
+                Response::AuthMessage {
+                    auth_message_type,
+                    auth_message,
+                } => Ok(AuthMessage::new_as_create_session(
+                    self_,
+                    auth_message_type,
+                    auth_message,
+                )),
+            })
+        }
     }
 }
 
-#[async_trait]
 impl<RW> StartableSession for RW
 where
     RW: TokioRW,
@@ -122,35 +108,39 @@ where
 {
     type Client = RW;
 
-    async fn start_session(
+    fn start_session(
         self,
         cmd: Vec<String>,
         env: Vec<String>,
-    ) -> super::Response<<Self as StartableSession>::Client, Self, <Self as StartableSession>::Client>
-    {
-        info!("Starting session with command {cmd:?}");
-        let ok = |t| Ok(Ok(t));
+    ) -> greetd_response!(
+        <Self as StartableSession>::Client,
+        <Self as StartableSession>::Client
+    ) {
+        async {
+            debug!("Starting session with command {cmd:?}",);
 
-        let (client, response) = make_request(self, Request::StartSession { cmd, env }).await?;
+            let ok = |t| Ok(Ok(t));
 
-        match response {
-            Response::Success => ok(client),
-            Response::Error {
-                error_type: ErrorType::AuthError,
-                description,
-            } => Ok(Err((client, super::RequestError::Auth(description)))),
-            Response::Error {
-                error_type: ErrorType::Error,
-                description,
-            } => Ok(Err((client, super::RequestError::Error(description)))),
-            Response::AuthMessage { .. } => unreachable!(
-                "greetd responded with auth request when starting an authenticated session"
-            ),
+            let (client, response) = make_request(self, Request::StartSession { cmd, env }).await?;
+
+            match response {
+                Response::Success => ok(client),
+                Response::Error {
+                    error_type: ErrorType::AuthError,
+                    description,
+                } => Ok(Err((client, super::RequestError::Auth(description)))),
+                Response::Error {
+                    error_type: ErrorType::Error,
+                    description,
+                } => Ok(Err((client, super::RequestError::Error(description)))),
+                Response::AuthMessage { .. } => unreachable!(
+                    "greetd responded with auth request when starting an authenticated session"
+                ),
+            }
         }
     }
 }
 
-#[async_trait]
 impl<T> CancellableSession for T
 where
     T: TokioRW,
@@ -160,6 +150,8 @@ where
     async fn cancel_session(
         self,
     ) -> super::Response<Self::Client, Self, <Self as CancellableSession>::Client> {
+        debug!("Cancelling session");
+
         let (client, response) = make_request(self, Request::CancelSession).await?;
 
         match response {
@@ -179,7 +171,6 @@ where
     }
 }
 
-#[async_trait]
 impl<RW> AuthResponse for AuthMessage<RW>
 where
     RW: TokioRW,
@@ -196,79 +187,81 @@ where
         }
     }
 
-    async fn respond(
+    fn respond(
         mut self,
         msg: Option<String>,
-    ) -> super::Response<
+    ) -> greetd_response!(
         <Self as AuthResponse>::Client,
-        Self,
-        CreateSessionResponse<<Self as AuthResponse>::Client>,
-    > {
-        let request = Request::PostAuthMessageResponse { response: msg };
-        if let Err(e) = request.write_to(&mut self.rw).await {
-            return Err((self, e));
+        CreateSessionResponse<<Self as AuthResponse>::Client>
+    ) {
+        async {
+            let request = Request::PostAuthMessageResponse { response: msg };
+            if let Err(e) = request.write_to(&mut self.rw).await {
+                return Err((self, e));
+            }
+
+            let response = match Response::read_from(&mut self.rw).await {
+                Ok(r) => r,
+                Err(e) => return Err((self, e)),
+            };
+
+            Ok(match response {
+                Response::Success => Ok(CreateSessionResponse::Success(self.rw)),
+                Response::Error {
+                    error_type: ErrorType::AuthError,
+                    description,
+                } => Err((self, super::RequestError::Auth(description))),
+                Response::Error {
+                    error_type: ErrorType::Error,
+                    description,
+                } => Err((self, super::RequestError::Error(description))),
+                Response::AuthMessage {
+                    auth_message_type,
+                    auth_message,
+                } => Ok(Self::new_as_create_session(
+                    self.rw,
+                    auth_message_type,
+                    auth_message,
+                )),
+            })
         }
-
-        let response = match Response::read_from(&mut self.rw).await {
-            Ok(r) => r,
-            Err(e) => return Err((self, e)),
-        };
-
-        Ok(match response {
-            Response::Success => Ok(CreateSessionResponse::Success(self.rw)),
-            Response::Error {
-                error_type: ErrorType::AuthError,
-                description,
-            } => Err((self, super::RequestError::Auth(description))),
-            Response::Error {
-                error_type: ErrorType::Error,
-                description,
-            } => Err((self, super::RequestError::Error(description))),
-            Response::AuthMessage {
-                auth_message_type,
-                auth_message,
-            } => Ok(Self::new_as_create_session(
-                self.rw,
-                auth_message_type,
-                auth_message,
-            )),
-        })
     }
 }
 
-#[async_trait]
 impl<RW> CancellableSession for AuthMessage<RW>
 where
     RW: TokioRW,
 {
     type Client = RW;
 
-    async fn cancel_session(
+    fn cancel_session(
         self,
-    ) -> super::Response<Self::Client, Self, <Self as CancellableSession>::Client> {
-        let result = match self.rw.cancel_session().await {
-            Ok(res) => res,
-            Err((not_canceled, e)) => {
-                return Err((
+    ) -> greetd_response!(Self::Client, <Self as CancellableSession>::Client) {
+        async {
+            let result = match self.rw.cancel_session().await {
+                Ok(res) => res,
+                Err((not_canceled, e)) => {
+                    return Err((
+                        Self {
+                            rw: not_canceled,
+                            ..self
+                        },
+                        e,
+                    ))
+                }
+            };
+
+            Ok(match result {
+                Ok(rw) => Ok(rw),
+                Err((not_canceled, e)) => Err((
                     Self {
                         rw: not_canceled,
                         ..self
                     },
                     e,
-                ))
-            }
-        };
-
-        Ok(match result {
-            Ok(rw) => Ok(rw),
-            Err((not_canceled, e)) => Err((
-                Self {
-                    rw: not_canceled,
-                    ..self
-                },
-                e,
-            )),
-        })
+                )),
+            })
+        }
     }
 }
 
