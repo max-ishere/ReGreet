87c6070c17aa982b47b7bd86351d152b48b1a850
max-ishere <47008271+max-ishere@users.noreply.github.com>
2024-10-07 13:45:28 +0300

    fix: (rfct regression) Reply to info & error immediately
    When greetd sends an info request, reply to it immediately and show the
    user the prompt associated with this request.

--- src/main.rs
+++ src/main.rs
@@ -111,9 +111,7 @@ async fn async_main(config: PathBuf, demo: bool, mut errors: Vec<NotificationIte
         config.commands.reboot = vec![];
         config.commands.poweroff = vec![];
 
-        let greetd_state = GreetdState::AuthQuestion {
-            session: DemoGreetd {},
-        };
+        let greetd_state = GreetdState::NotCreated(DemoGreetd {});
 
         app.run::<App<DemoGreetd>>(mk_app_init(greetd_state, cache, users, config, errors));
 
